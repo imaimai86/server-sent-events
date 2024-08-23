a9aa44addaa4562b19599b92bb8971b1836ff32a
Anas <muhammed.anas@medable.com>
2024-08-23 12:45:29 +0530

    Logwatch complete

--- sse-server/server.js
+++ sse-server/server.js
@@ -1,117 +1,94 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const fs = require('fs');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs");
+const { processLineByLine } = require("./utils");
+const Tail = require("tail").Tail;
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.get('/status', (request, response) => response.json({clients: clients.length}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = 3001;
 
-let clients = [];
 const clientMap = new Map();
-let facts = [];
 
 app.listen(PORT, () => {
-  console.log(`Facts Events service listening at http://localhost:${PORT}`)
-})
+  console.log(`Facts Events service listening at http://localhost:${PORT}`);
+});
 
+app.get("/status", (request, response, next) =>
+  response.json({ clients: clientMap.size })
+);
 
+async function eventsHandler(request, response, next) {
+  const headers = {
+    "Content-Type": "text/event-stream",
+    Connection: "keep-alive",
+    "Cache-Control": "no-cache",
+  };
+  response.writeHead(200, headers);
 
-// ...
+  const clientId = request.params.requestId;
+  console.log(`${clientId} Connection opened`);
 
-function eventsHandler(request, response, next) {
-    const headers = {
-      'Content-Type': 'text/event-stream',
-      'Connection': 'keep-alive',
-      'Cache-Control': 'no-cache'
-    };
-    response.writeHead(200, headers);
-  
-    const data = `data: ${JSON.stringify(facts)}\n\n`;
-  
-    response.write(data);
-  
-    const clientId = request.params.requestId;
-  
-    const newClient = {
-      id: clientId,
-      response
-    };
+  const filename = `Test_${clientId}.log`;
+  const path = `./logs/${filename}`;
+  fs.writeFileSync(path, `STARTS HERE\n`);
 
-    clientMap.set(clientId, newClient);
-    clients.push(newClient);
-  
-    request.on('close', () => {
-      console.log(`${clientId} Connection closed`);
+  const logWatch = await watchLogFile(clientId, path);
+
+  const newClient = {
+    id: clientId,
+    response,
+    logWatch,
+  };
+
+  clientMap.set(clientId, newClient);
+  console.log("Number of connections", clientMap.size, clientMap.keys());
+
+  request.on("close", () => {
+    const client = clientMap.get(clientId);
+    if (!!client) {
+      !!client.response && client.response.end();
+      !!client.logWatch && client.logWatch.unwatch();
       clientMap.delete(clientId);
-      clients = clients.filter(client => client.id !== clientId);
-    });
-
-    const filename = `Test_${clientId}.log`;
-    const path = `./${filename}`;
-    //if file does not exists, create it
-    if (!fs.existsSync(path)) {
-        // fs.writeFileSync(path, '{"info":"Show 1","source":"Anas"}');
+      console.log("Number of connections", clientMap.size);
     }
-    //add contents to the end of the file in a newline
-// fs.writeFileSync
+  });
+}
 
-    // fs.appendFileSync(path, `\r\n{"info":"Show 1","source":"Anas"}`);
-    // fs.appendFileSync(path, `\r\n{"info":"Show 2","source":"Anas"}`);
-    // fs.appendFileSync(path, `\r\n{"info":"Show 3","source":"Anas"}`);
+app.get("/events/:requestId", eventsHandler);
 
-    tailFile(clientId, path);
-  }
-  
-  app.get('/events/:requestId', eventsHandler);
-
-
-  // ...
-
-  function sendEventsToAll(newFact) {
-    clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
+async function sendEventsToClient(clientId, event) {
+  const client = clientMap.get(clientId);
+  console.log("clientMap RETRIEVED", !!client);
+  if (!event || !event.length) {
+    return;
   }
 
-  function sendEventsToClient(clientId, event) {
-    const client = clientMap.get(clientId);
-    client.response.write(`data: ${JSON.stringify(event)}\n\n`)
-  }
-  
-  async function addFact(request, response, next) {
-    const newFact = request.body;
-    const clientId = request.params.requestId;
-    facts.push(newFact);
-    response.json(newFact)
-    return sendEventsToAll(newFact);
-  }
-  
-  app.post('/fact/:requestId', addFact);
+  console.log("Send DATA", event);
+  client.response.write(`data: {"info":"${event}","source":"Anas"}\n\n`);
+}
 
-/**
- * function to tail contents of a file to a client, client is identified by clientId from clientMap, filename is Test_${clientId}.log, End the stream when file returns string ==END==
- */
-async function tailFile(clientId, path) {
-    // const filename = `Test_${clientId}.log`;
-    if (!fs.existsSync(path)) {
-      return;
-    }
-    const fileStream = fs.createReadStream(path)
-    const client = clientMap.get(clientId);
-    fileStream.on('data', (chunk) => {
-      console.log('chunk', new Buffer(chunk).toString('utf8'));
-      client.response.write(`data: ${chunk}\n\n`);
-    });
-    fileStream.on('end', () => {
-      client.response.write(`data: {"info":"==END==","source":"Anas"}\n\n`);
-      client.response.end();
-    });
+async function watchLogFile(clientId, path) {
+  const tail = new Tail(path, { fromBeginning: true });
 
-    // read  from file $path line by line
+  tail.on("line", async (data) => {
+    if (!data) return;
+    // console.log('clientId', clientId);
+    // await sendEventsToClient(clientId, data);
+    console.log("On line", data);
+    sendEventsToClient(clientId, data);
+  });
 
+  tail.on("error", (error) => {
+    console.log("error", error);
+  });
+
+  tail.watch();
+
+  return tail;
 }
